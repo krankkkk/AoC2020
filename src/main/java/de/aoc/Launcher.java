999ec13c6ff74c7701af9272fcb43a1fa80100a6
krankkkk <noreply+192386193@github.com>
2021-11-24 02:57:33 +0100

    Test-Push2

--- src/main/java/de/aoc/Launcher.java
+++ src/main/java/de/aoc/Launcher.java
@@ -7,6 +7,7 @@ public class Launcher {
 
     public static void main(String[] args) {
         final Day day12 = new Day12();
+        //
         System.out.printf("%d%n%d%n", day12.part1(), day12.part2());
     }
 }
